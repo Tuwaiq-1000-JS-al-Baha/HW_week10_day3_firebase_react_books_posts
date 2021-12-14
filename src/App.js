b376f88e3c2315b6747ec07c6b20844c2aa2eccc
lalama2000 <92187017+lalama2000@users.noreply.github.com>
2021-12-14 22:04:28 +0300

    DONE✅

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { toast, ToastContainer } from "react-toastify"
 import { Route, Routes, useNavigate } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import AddPost from "./pages/AddPost"
+import AddBook from "./pages/AddBook"
 import Posts from "./pages/Posts"
 import firebase from "./utils/firebase"
 import PostsContext from "./utils/PostsContext"
-
+import Book from "./pages/Book"
 function App() {
   const [posts, setPosts] = useState([])
+  const [books, setBooks] = useState([])
   const navigate = useNavigate()
 
   const getPosts = async () => {
@@ -21,9 +23,21 @@ function App() {
 
     setPosts(postsArray)
   }
+  //get all books
+  const getBooks = async () => {
+    const booksRef = await firebase.database().ref("books").once("value")
+    const books = booksRef.val()
+    const booksArray = []
+    for (const key in books) {
+      booksArray.push({ ...books[key], id: key })
+    }
+
+    setBooks(booksArray)
+  }
 
   useEffect(() => {
     getPosts()
+    getBooks()
   }, [])
 
   const addPost = async e => {
@@ -40,12 +54,32 @@ function App() {
       await firebase.database().ref("/posts").push(postBody)
       toast.success("post added")
       getPosts()
-      navigate("/")
+      navigate("/post")
     } catch (error) {
       toast.error(error.code)
     }
   }
+  //add books
+  const addBook = async e => {
+    e.preventDefault()
+    try {
+      const form = e.target
+      const bookBody = {
+        title: form.elements.title.value,
+        body: form.elements.body.value,
+        image: form.elements.image.value,
+        owner: form.elements.owner.value,
+      }
 
+      await firebase.database().ref("/books").push(bookBody)
+      toast.success("book added")
+      getBooks()
+      navigate("/book")
+    } catch (error) {
+      toast.error(error.code)
+    }
+  }
+  //delete post
   const deletePost = async postId => {
     try {
       await firebase.database().ref(`posts/${postId}`).remove()
@@ -75,15 +109,17 @@ function App() {
     }
   }
 
-  const store = { posts, addPost, deletePost, editPost }
+  const store = { posts, books, addPost, addBook, deletePost, editPost }
 
   return (
     <PostsContext.Provider value={store}>
       <ToastContainer />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Posts />} />
+        <Route path="/post" element={<Posts />} />
+        <Route path="/book" element={<Book />} />
         <Route path="/add-post" element={<AddPost />} />
+        <Route path="/add-book" element={<AddBook />} />
       </Routes>
     </PostsContext.Provider>
   )
