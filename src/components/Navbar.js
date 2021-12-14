1b540e92557a5ac57571ce0bbd1a12742783c9a7
Arwa94a <92246034+Arwa94a@users.noreply.github.com>
2021-12-14 08:32:10 -0800

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,19 @@ function NavbarItem() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Link to="/" className="navbar-brand">
-          Posts
-        </Link>
-        <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="me-auto">
-            <Link to="/" className="nav-link">
+            <Link to="/posts" className="nav-link">
               Posts
             </Link>
             <Link to="/add-post" className="nav-link">
               Add Post
+            </Link>
+            <Link to="/books" className="nav-link">
+              Books
+            </Link>
+            <Link to="/add-book" className="nav-link">
+              Add Book
             </Link>
           </Nav>
         </Navbar.Collapse>
---
+++ src/pages/Books.js
@@ -0,0 +1,42 @@
+import { useContext, useState } from "react"
+import PostsContext from "../utils/PostsContext"
+import { Button, Card, Col, Row } from "react-bootstrap"
+import PostEditModal from "../components/PostEditModal"
+import BookEditModal from "../components/BookEditModal"
+
+function Books() {
+  const { books, deleteBook } = useContext(PostsContext)
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      <h1>Books</h1>
+      <Row cols={4}>
+        {books.map(book => (
+          <Col>
+            <Card>
+              <Card.Header>
+                <Card.Title>title:{book.title}</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>ReleaseYear: {book.releaseYear}</Card.Text>
+                <Card.Img src={book.image} style={{ width: 200 }} />
+                <Card.Text>number Of Copies:{book.numberOfCopies}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button variant="success" className="me-2" onClick={() => setShow(true)}>
+                  Edit
+                </Button>
+                <Button variant="danger" onClick={() => deleteBook(book.id)}>
+                  Delete
+                </Button>
+              </Card.Footer>
+            </Card>
+            <BookEditModal show={show} setShow={setShow} book={book} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default Books
